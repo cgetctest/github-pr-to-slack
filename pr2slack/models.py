7355031fbdd9d72d961ed922d2dcc425e2b46e85
cgetc <shigetoshi.komatsu@mixi.co.jp>
2017-07-25 21:30:48 +0900

    Threadのmodelを定義

--- pr2slack/models.py
+++ pr2slack/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class PullReqeustThread(models.Model):
+    channel = models.TextField()
+    thread = models.TextField()
+    repository = models.TextField()
+    pull_request = models.TextField()
+
+    class Meta:
+        db_table = 'p2t_pull_reqeust_thread'
+        unique_together = (
+            ('channel', 'thread'),
+            ('repository', 'pull_request'),
+        )
